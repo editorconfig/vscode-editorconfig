2ab6a7e34334597b8571f9417985f0de2b641f54
Renato Dinhani Conceição <renatodinhani@gmail.com>
2017-03-02 15:12:51 -0300

    unset  value for all properties

--- src/EditorConfigCompletionProvider.ts
+++ src/EditorConfigCompletionProvider.ts
@@ -4,14 +4,14 @@ import { TextDocument, Range, Position } from 'vscode'
 class EditorConfigCompletionProvider implements CompletionItemProvider {
 
 	private readonly properties: Property[] = [
-		new Property('root', ['true', 'false'], 'Special property that should be specified at the top of the file outside of any sections. Set to true to stop .editorconfig files search on current file.'),
-		new Property('charset', ['utf-8', 'utf-8-bom', 'utf-16be', 'utf-16le', 'latin1'], 'Set to latin1, utf-8, utf-8-bom, utf-16be or utf-16le to control the character set. Use of utf-8-bom is discouraged.'),
-		new Property('end_of_line', ['lf', 'cr', 'crlf'], 'Set to lf, cr, or crlf to control how line breaks are represented.'),
-		new Property('indent_style', ['tab', 'space'], 'Set to tab or space to use hard tabs or soft tabs respectively.'),
-		new Property('indent_size', ['1', '2', '3', '4', '5', '6', '7', '8'], ' A whole number defining the number of columns used for each indentation level and the width of soft tabs (when supported). When set to tab, the value of tab_width (if specified) will be used.'),
-		new Property('insert_final_newline', ['true', 'false'], 'Set to true to ensure file ends with a newline when saving and false to ensure it doesn\'t.'),
-		new Property('tab_width', ['1', '2', '3', '4', '5', '6', '7', '8'], 'A whole number defining the number of columns used to represent a tab character. This defaults to the value of indent_size and doesn\'t usually need to be specified.'),
-		new Property('trim_trailing_whitespace', ['true', 'false'], 'Set to true to remove any whitespace characters preceding newline characters and false to ensure it doesn\'t.')
+		new Property('root', ['true', 'false', 'unset'], 'Special property that should be specified at the top of the file outside of any sections. Set to true to stop .editorconfig files search on current file.'),
+		new Property('charset', ['utf-8', 'utf-8-bom', 'utf-16be', 'utf-16le', 'latin1', 'unset'], 'Set to latin1, utf-8, utf-8-bom, utf-16be or utf-16le to control the character set. Use of utf-8-bom is discouraged.'),
+		new Property('end_of_line', ['lf', 'cr', 'crlf', 'unset'], 'Set to lf, cr, or crlf to control how line breaks are represented.'),
+		new Property('indent_style', ['tab', 'space', 'unset'], 'Set to tab or space to use hard tabs or soft tabs respectively.'),
+		new Property('indent_size', ['1', '2', '3', '4', '5', '6', '7', '8', 'unset'], ' A whole number defining the number of columns used for each indentation level and the width of soft tabs (when supported). When set to tab, the value of tab_width (if specified) will be used.'),
+		new Property('insert_final_newline', ['true', 'false', 'unset'], 'Set to true to ensure file ends with a newline when saving and false to ensure it doesn\'t.'),
+		new Property('tab_width', ['1', '2', '3', '4', '5', '6', '7', '8', 'unset'], 'A whole number defining the number of columns used to represent a tab character. This defaults to the value of indent_size and doesn\'t usually need to be specified.'),
+		new Property('trim_trailing_whitespace', ['true', 'false', 'unset'], 'Set to true to remove any whitespace characters preceding newline characters and false to ensure it doesn\'t.')
 	];
 
 	// =========================================================================
